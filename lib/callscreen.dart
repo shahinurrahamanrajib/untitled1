2115b670a67c1c4f6073f19b5a545c23dbd593a9
USER <srrajib61@gmail.com>
2021-10-20 23:51:36 +0600

    initikkih

--- lib/callscreen.dart
+++ lib/callscreen.dart
@@ -21,10 +21,19 @@ class CallScreen extends StatelessWidget {
           Positioned(
             bottom: -50,
             right: 0,
-            left: 10,
+
             child: CircleAvatar(
               radius: 60,
               backgroundColor: Colors.white,
+            ),
+          ),
+          Positioned(
+            bottom: -50,
+            // left: 119,
+            child: CircleAvatar(
+              child: Icon(Icons.play_arrow_outlined,color: Colors.white,size: 40,),
+              radius: 61,
+              backgroundColor: Colors.blue,
             ),
           ),
         ],
