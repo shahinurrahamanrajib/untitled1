ddbd81dc808ded271196e2d8eb9324c1d1b7ab91
USER <srrajib61@gmail.com>
2021-10-22 00:40:30 +0600

    initikkryk

--- lib/callscreen.dart
+++ lib/callscreen.dart
@@ -8,39 +8,39 @@ class CallScreen extends StatelessWidget {
     return Scaffold(
 
       backgroundColor: Colors.black,
-      body: Stack(
-        alignment: Alignment.bottomCenter,
-        clipBehavior: Clip.none,
-        children: <Widget>[
+      body: SafeArea(
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          clipBehavior: Clip.none,
+          children: <Widget>[
 
 
-          Positioned(
-            top: MediaQuery.of(context).size.height/11.5,
-            right: 50,
-            left: 50,
-            child: Center(
-              child: CircleAvatar(
-                backgroundColor: Colors.white10,
-                child:  Positioned(
-                  top: MediaQuery.of(context).size.height/10,
-                  right: 50,
-                  left: 50,
-                  child: Center(
-                    child: CircleAvatar(
-                      radius: 100,
-                      backgroundImage: NetworkImage(
-                          'https://avatars.githubusercontent.com/u/92436730?s=400&u=d1dd15e87de30c39dc74abfa026bb959c65598d6&v=4'),
+            Positioned(
+              top: MediaQuery.of(context).size.height/11.5,
+              right: 50,
+              left: 50,
+              child: Center(
+                child: CircleAvatar(
+                  backgroundColor: Colors.white60,
+                  child:  Positioned(
+                    top: MediaQuery.of(context).size.height/10,
+                    right: 50,
+                    left: 50,
+                    child: Center(
+                      child: CircleAvatar(
+                        radius: 100,
+                        backgroundImage: NetworkImage(
+                            'https://avatars.githubusercontent.com/u/92436730?s=400&u=d1dd15e87de30c39dc74abfa026bb959c65598d6&v=4'),
+                      ),
                     ),
                   ),
+                  radius: 110,
                 ),
-                radius: 110,
               ),
             ),
-          ),
 
-
-
-        ],
+          ],
+        ),
       )
 
     );
