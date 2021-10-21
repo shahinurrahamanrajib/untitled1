43e66b41253587f72b77e779db97845a6dfd792a
USER <srrajib61@gmail.com>
2021-10-22 01:17:19 +0600

    initikkryk

--- lib/callscreen.dart
+++ lib/callscreen.dart
@@ -8,39 +8,66 @@ class CallScreen extends StatelessWidget {
     return Scaffold(
 
       backgroundColor: Colors.black,
-      body: SafeArea(
-        child: Stack(
-          alignment: Alignment.bottomCenter,
-          clipBehavior: Clip.none,
-          children: <Widget>[
-
-
-            Positioned(
-              top: MediaQuery.of(context).size.height/11.5,
-              right: 50,
-              left: 50,
-              child: Center(
-                child: CircleAvatar(
-                  backgroundColor: Colors.white60,
-                  child:  Positioned(
-                    top: MediaQuery.of(context).size.height/10,
-                    right: 50,
-                    left: 50,
-                    child: Center(
-                      child: CircleAvatar(
-                        radius: 100,
-                        backgroundImage: NetworkImage(
-                            'https://avatars.githubusercontent.com/u/92436730?s=400&u=d1dd15e87de30c39dc74abfa026bb959c65598d6&v=4'),
-                      ),
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        clipBehavior: Clip.none,
+        children: <Widget>[
+          Positioned(
+            top: MediaQuery.of(context).size.height/11.5,
+            right: 50,
+            left: 50,
+            child: Center(
+              child: CircleAvatar(
+                backgroundColor: Colors.white60,
+                child:  Positioned(
+                  top: MediaQuery.of(context).size.height/10,
+                  right: 50,
+                  left: 50,
+                  child: Center(
+                    child: CircleAvatar(
+                      radius: 100,
+                      backgroundImage: NetworkImage(
+                          'https://avatars.githubusercontent.com/u/92436730?s=400&u=d1dd15e87de30c39dc74abfa026bb959c65598d6&v=4'),
                     ),
                   ),
-                  radius: 110,
                 ),
+                radius: 110,
               ),
             ),
+          ),
+          Center(
+            child: Container(
+              height: 150,
+              // width: 1000,
+              color: Colors.transparent,
+              child: Center(child: Text("Shahinur Rahaman",
+                style: TextStyle(color: Colors.white,fontSize: 30),
+              )),
+            ),
+          ),
+          Positioned(
 
-          ],
-        ),
+            top: MediaQuery.of(context).size.height/1.28,
+            left: 20,
+            child: CircleAvatar(
+              child: Icon(Icons.call_end_rounded,color: Colors.white,size: 45,),
+              radius: 45,
+              backgroundColor: Colors.red,
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height/1.28,
+            right: 20,
+            child: CircleAvatar(
+              child: Icon(Icons.call_rounded,color: Colors.white,size: 45,),
+              radius: 45,
+              backgroundColor: Colors.green,
+            ),
+          ),
+
+
+
+        ],
       )
 
     );
