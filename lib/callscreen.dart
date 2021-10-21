f7cb8068ce3b7bfe95d4e98d89cb9e8bec9e769f
USER <srrajib61@gmail.com>
2021-10-22 00:33:51 +0600

    initikkryk

--- lib/callscreen.dart
+++ lib/callscreen.dart
@@ -6,25 +6,38 @@ class CallScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow,
+
+      backgroundColor: Colors.black,
       body: Stack(
         alignment: Alignment.bottomCenter,
         clipBehavior: Clip.none,
         children: <Widget>[
-          Container(
-            height: 400,
-            // width: 1000,
-            color: Colors.blue,
-          ),
+
+
           Positioned(
-            bottom: -50,
+            top: MediaQuery.of(context).size.height/11.5,
             right: 50,
             left: 50,
-            child: CircleAvatar(
-              radius: 60,
-              backgroundColor: Colors.white,
+            child: Center(
+              child: CircleAvatar(
+                backgroundColor: Colors.white10,
+                child:  Positioned(
+                  top: MediaQuery.of(context).size.height/10,
+                  right: 50,
+                  left: 50,
+                  child: Center(
+                    child: CircleAvatar(
+                      radius: 100,
+                      backgroundImage: NetworkImage(
+                          'https://avatars.githubusercontent.com/u/92436730?s=400&u=d1dd15e87de30c39dc74abfa026bb959c65598d6&v=4'),
+                    ),
+                  ),
+                ),
+                radius: 110,
+              ),
             ),
           ),
+
 
 
         ],
